2eccc5ae2e9f4181f2d654c60a5b6970169fae8e
GeniusK <geniusk1047@naver.com>
2019-09-12 14:50:53 +0900

    shortUrl로 접근 시 예외 처리
    - shortUrl이 9자리가 아닐경우
    - 존재하지 않는 shortUrl이 올 경우

--- handler/accessShortUrl.js
+++ handler/accessShortUrl.js
@@ -10,6 +10,19 @@ const URL = require('../model/url')
  */
 module.exports = async (req, res) => {
   const { shortUrl } = req.params
+  if (shortUrl.length !== 9) {
+    // create 시 nanoId를 9자리로만 생성하기 때문에 9자리 이외의 자릿수는 존재 불가
+    return res.status(400).json({
+      errorCode: 'ValidationError',
+      errorMessage: '짧게 만든 주소는 9자리로 입력해주세요.'
+    })
+  }
   const urlData = await URL.findOne({ shortUrl })
+  if (!urlData) {
+    return res.status(400).json({
+      errorCode: 'ValidationError',
+      errorMessage: '존재하지 않는 URL 입니다.'
+    })
+  }
   return res.redirect(urlData.originUrl)
 }
