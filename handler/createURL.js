f7ae738900cdf1e310cf428fe5c8b85e0513dfc0
GeniusK <geniusk1047@naver.com>
2019-09-12 16:47:35 +0900

    10000개 이상이면 가장 사용 안했던 데이터 삭제

--- handler/createURL.js
+++ handler/createURL.js
@@ -1,6 +1,7 @@
 'use strict'
 const nanoId = require('nanoid')
 const URL = require('../model/url')
+const UrlStats = require('../model/urlStats')
 
 module.exports = async (req, res) => {
   const { url } = req.query
@@ -10,6 +11,13 @@ module.exports = async (req, res) => {
       errorCode: 'ValidationError',
       errorMessage: 'URL을 형식에 맞추어 입력해주세요.'
     })
+  }
+  // URL 데이터가 10000개 이상이면 제일 오래된 데이터 삭제
+  const docCount = await URL.countDocuments()
+  if (docCount === 10000) {
+    const { shortUrl } = await URL.findOne().sort({ useDate: 1 })
+    await URL.remove({ shortUrl })
+    await UrlStats.remove({ shortUrl })
   }
   try {
     const result = await new URL({
